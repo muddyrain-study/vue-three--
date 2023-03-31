78674d84c7b6993804400aac2267e40f1b65657b
“muddyrain” <qiu3291002845@gmail.com>
2023-03-31 10:12:48 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script setup>
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+// 创建场景
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.set(0, 2, 5.5);
+
+const renderer = new THREE.WebGLRenderer({
+  // 抗锯齿
+  antialias: true,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+requestAnimationFrame(function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+});
+
+const axis = new THREE.AxesHelper(5);
+scene.add(axis);
+
+// 加载全景图
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+canvas {
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  left: 0;
+  top: 0;
+}
+</style>
